2e37d79a73fa1998c304bd30d7b4d0fe9f5a3f61
Mike Erickson <codedungeon@gmail.com>
2014-06-26 15:53:16 -0700

    integrated behat options

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /*jshint node:true */
 
 /* options
-* --format=pretty|progress|html|failed
-^ --ansi (default)
-* --no-ansi  (uses ansi:false)
-* --no-time  (uses time:true)
-*/
+ * --format=pretty|progress|html|failed
+ ^ --ansi (default)
+ * --no-ansi  (uses ansi:false)
+ * --no-time  (uses time:true)
+ */
 
 'use strict';
 
 var map   = require('map-stream'),
-    gutil = require('gulp-util'),
-    os    = require('os'),
-    exec  = require('child_process').exec;
+	gutil = require('gulp-util'),
+	os    = require('os'),
+	exec  = require('child_process').exec;
 
 module.exports = function(command, opt){
 	var counter = 0;
@@ -24,6 +24,8 @@ module.exports = function(command, opt){
 
 	// if path to behat bin not supplied, use default vendor/bin path
 	if(! command) {
+		var ansi = '--ansi';
+
 		command = './vendor/bin/behat';
 		if (os.platform() === 'win32') {
 			command = '.\\vendor\\bin\\behat';
@@ -38,30 +40,39 @@ module.exports = function(command, opt){
 	if (typeof opt.clear === 'undefined') { opt.clear = false; }
 	if (typeof opt.flags === 'undefined') { opt.flags = ''; }
 	if (typeof opt.silent === 'undefined') { opt.silent = false; }
-	if (typeof opt.test === 'undefined') { opt.silent = true; option.debug = true}
+	if (typeof opt.test === 'undefined') { opt.silent = true; opt.debug = true}
 	if (typeof opt.development === 'undefined') { opt.development = false; }
 
 	// Behat Options: specific to Behat */
-	if (typeof opt.format === 'undefined') { opt.format = ''; }       // --format='format'
-	if (typeof opt.features === 'undefined') { opt.features = ''; }   // features='features'
-	if (typeof opt.showTime === 'undefined') { opt.showTime = true; } // ==no-time
-	if (typeof opt.ansi === 'undefined') { opt.ansi = true; }         // --no-ansi
-	if (typeof opt.showPaths === 'undefined') { opt.showPaths = true; } // --no-paths
-	if (typeof opt.multiline === 'undefined') { opt.multiline = true; } // ==multiline
-	if (typeof opt.expand === 'undefined') { opt.expand = true; } // --no-expand
-	if (typeof opt.definitions === 'undefined') { opt.definitions = '-d'; } // ==definitions="=d"
-	if (typeof opt.name === 'undefined') { opt.name = ''; }  // --name
-	if (typeof opt.tags === 'undefined') { opt.tags = ''; }  // --tags
-	if (typeof opt.strict === 'undefined') { opt.strict = false; } // --strict
-	if (typeof opt.stopOnFail === 'undefined') { opt.stopOnFail = false; } // =-stop-on-failure
-	if (typeof opt.configFile === 'undefined') { opt.configFile = ''; } // ==config='file'
-	if (typeof opt.profile === 'undefined') { opt.profile = ''; } // --profile='profile'
-
+	if (typeof opt.format === 'undefined') { opt.format = ''; }             // --format='format'
+	if (typeof opt.features === 'undefined') { opt.features = ''; }         // features='features'
+	if (typeof opt.showTime === 'undefined') { opt.showTime = true; }       // --no-time
+	if (typeof opt.ansi === 'undefined') { opt.ansi = true; }               // --no-ansi
+	if (typeof opt.showPaths === 'undefined') { opt.showPaths = true; }     // --no-paths
+	if (typeof opt.multiline === 'undefined') { opt.multiline = true; }     // --multiline
+	if (typeof opt.expand === 'undefined') { opt.expand = true; }           // --no-expand
+	if (typeof opt.definitions === 'undefined') { opt.definitions = '-d'; } // --definitions="=d"
+	if (typeof opt.name === 'undefined') { opt.name = ''; }                 // --name
+	if (typeof opt.tags === 'undefined') { opt.tags = ''; }                 // --tags
+	if (typeof opt.strict === 'undefined') { opt.strict = false; }          // --strict
+	if (typeof opt.stopOnFail === 'undefined') { opt.stopOnFail = false; }  // --stop-on-failure
+	if (typeof opt.configFile === 'undefined') { opt.configFile = ''; }     // --config='file'
+	if (typeof opt.profile === 'undefined') { opt.profile = ''; }           // --profile='profile'
 
 	return map(function (file, cb) {
 
 		// construct command
 		var cmd = opt.clear ? 'clear && ' + command : command;
+
+		// integrate options
+		cmd = opt.ansi ? cmd += ' --ansi' : cmd += ' --no-ansi';
+		cmd = opt.expand ? cmd += ' --expand' : cmd += ' --no-expand';
+		cmd = opt.showTime ? cmd += ' --time' : cmd += ' --no-time';
+		cmd = opt.showPaths ? cmd += ' --paths' : cmd += ' --no-paths';
+		if(opt.strict) { cmd += '--strict' }
+		if(opt.stopOnFail) { cmd += '--stop-on-failure' }
+		if(opt.configFile != '') { cmd += '--config=' + opt.configFile }
+		if(opt.profile != '') { cmd += '--config=' + opt.profile }
 
 		if(counter === 0) {
 			counter++;
@@ -77,27 +88,30 @@ module.exports = function(command, opt){
 			if ( ! opt.development) {
 				exec(cmd, function (error, stdout, stderr) {
 
-				if (!opt.silent && stderr) {
-					gutil.log(stderr);
-				}
+					console.log(stdout);
 
-				if (stdout) {
-					stdout = stdout.trim(); // Trim trailing cr-lf
-				}
+					if (!opt.silent && stderr) {
+						gutil.log(stderr);
+					}
 
-				if (!opt.silent && stdout) {
-					gutil.log(stdout);
-				}
+					if (stdout) {
+						stdout = stdout.trim(); // Trim trailing cr-lf
+					}
 
-				if(opt.debug && error) {
-					gutil.log(error);
-				}
+					if (!opt.silent && stdout) {
+						gutil.log(stdout);
+					}
 
-				if (opt.notify) {
-					cb(error, file);
-				}
+					if (opt.debug && error) {
+						gutil.log(error);
+					}
+
+					if (opt.notify) {
+						cb(error, file);
+					}
+
+				});
 			}
-			});
 		}
 	});
 
